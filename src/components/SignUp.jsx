1a760f0fff8472e529a7f3795e5cae27d1459b5a
Vedant <bhavsarvedant9@gmail.com>
2026-02-01 13:19:40 +0530

    post form, RTE

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -34,7 +34,7 @@ const SignUp = () => {
         className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
       >
         <div className="mb-2 flex justify-center">
-          <span className="inline-block w-full max-w-[100px]">
+          <span className="inline-block w-full max-w-[100 px]">
             <Logo width="100%" />
           </span>
         </div>
